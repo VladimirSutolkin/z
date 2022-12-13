bc3a4af9eacc486aac85b937c72231d3c93ef460
VladimirSutolkin <sutolkinv@gmail.com>
2022-12-14 00:37:16 +0300

    Commit T

---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,24 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    private static final UserService userService = new UserServiceImpl();
+    private static final User user1 = new User("Vladimir", "Sutolkin", (byte) 99);
+    private static final User user2 = new User("Vladimir", "Sutolkin", (byte) 98);
+    private static final User user3 = new User("Vladimir", "Sutolkin", (byte) 97);
+    private static final User user4 = new User("Vladimir", "Sutolkin", (byte) 96);
+    public static void main(String[] args) {
+        userService.createUsersTable();
+        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
+        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
+        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
+        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
+    }
+}
---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,25 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Polpa12siM";
+    private static final String URL = "jdbc:mysql://localhost:3306?autoReconnect=true&useSSL=false";
+
+    public static Connection getConnection (){
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+}
